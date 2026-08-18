24f897deabfa62598299c06fd5cf24404339bd15
Damien Lamont <damien.lamont@fullstack.co.za>
2026-08-18 11:21:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CarForm from "./components/CarForm.js";
+import CarList from "./components/CarList.js";
+import CarSearch from "./components/CarSearch.js";
+import CarValue from "./components/CarValue.js";
+
+function App() {
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
+}
+
+export default App;
